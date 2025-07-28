01029d49589d50fa73e1afdd95df111969cb434f
Sameer Khokhar <skkhokhar787@gmail.com>
2025-07-28 21:55:05 +0500

    Add ReverseStack class to reverse a stack and print its elements

---
+++ src/ReverseStack.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+public class ReverseStack {
+    public static void pushAtBottom (int data, Stack<Integer> s) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+    }
+
+    public static void reverse(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(top,s);
+
+    }
+
+    public static void print(Stack<Integer> s) {
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+        System.out.println();
+    }
+
+    public static void main (String [] args) {
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        // Print before
+        reverse(s);
+        print(s);
+
+
+    }
+}
